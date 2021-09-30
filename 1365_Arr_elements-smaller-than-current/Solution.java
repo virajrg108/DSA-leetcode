f8083240d96b9e52788329b01b233395e3884823
virajrg108 <vrgawde_b16@et.vjti.ac.in>
2021-09-30 14:28:47 +0530

    Code#1 : Number of array element smaller than current element

---
+++ 1365_Arr_elements-smaller-than-current/Solution.java
@@ -0,0 +1,19 @@
+// https://github.com/virajrg108/DSA-leetcode.git
+
+// 1365. How Many Numbers Are Smaller Than the Current Number
+
+class Solution {
+  public int[] smallerNumbersThanCurrent(int[] nums) {
+    int[] ans = new int[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      int cnt = 0;
+      for (int j = 0; j < nums.length; j++) {
+        if (i != j && (nums[j] < nums[i])) {
+          cnt++;
+        }
+        ans[i] = cnt;
+      }
+    }
+    return ans;
+  }
+}
